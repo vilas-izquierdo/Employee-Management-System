bbeeba650819072eb42fd3b8125cab778e5a12ed
Vilasa Izquierdo <vilasi@gmail.com>
2022-05-03 22:15:27 -0700

    added gets to view data

--- db/schema.sql
+++ db/schema.sql
@@ -4,27 +4,25 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT NOT NULL,
-    deparment_name VARCHAR(30),
-    PRIMARY KEY (id)
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    deparment_name VARCHAR(30)
 );
 
 CREATE TABLE roles (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
-    FOREIGN KEY (department_id) REFERENCES department(id),
-    PRIMARY KEY (id)
+    FOREIGN KEY (department_id) REFERENCES department(id)
+
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES department(id),
-    PRIMARY KEY (id)
+    FOREIGN KEY (manager_id) REFERENCES employee(id)  
 );
--- server.js
+++ server.js
@@ -17,6 +17,58 @@ const db = mysql.createConnection(
   );
   
 
+app.get('/api/departments', (req, res) => {
+
+    const sql = `SELECT * FROM department`;
+    
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+  app.get('/api/employees', (req, res) => {
+
+    const sql = `SELECT * from employee`;
+    
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+  app.get('/api/roles', (req, res) => {
+
+    const sql = `SELECT * from roles`;
+    
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+         return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      });
+    });
+  });
+
+
+
+
+
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
     });
