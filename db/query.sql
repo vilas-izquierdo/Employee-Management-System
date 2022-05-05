3cf5812673d07cc7a8bbef79238582a58de64170
Vilasa Izquierdo <vilasi@gmail.com>
2022-05-05 15:06:36 -0700

    added query sql

---
+++ db/query.sql
@@ -0,0 +1,20 @@
+SELECT * FROM department;
+
+
+SELECT
+employee.id, 
+employee.first_name, 
+employee.last_name, 
+roles.title, 
+roles.salary, 
+employee.manager_id
+FROM employee
+INNER JOIN roles ON roles.id = employee.roles_id
+INNER JOIN department ON department.id = roles.department_id
+ORDER BY employee.id;
+
+SELECT
+roles.id, roles.title, deparment.name, roles.salary
+FROM roles 
+JOIN department ON roles.department_id = department.id
+ORDER BY roles.id;
--- db/schema.sql
+++ db/schema.sql
@@ -4,23 +4,24 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     deparment_name VARCHAR(30)
 );
 
 CREATE TABLE roles (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     department_id INT,
     FOREIGN KEY (department_id) REFERENCES department(id)
+    ON DELETE SET NULL
 
 );
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES roles(id),
